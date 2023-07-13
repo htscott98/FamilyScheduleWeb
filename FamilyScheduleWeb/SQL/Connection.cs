294cc3fd9f8b7ed00f67aae0ff45d0401d123437
htscott98 <htscott98@hotmail.com>
2023-07-13 16:32:54 -0400

    Add project files.

---
+++ FamilyScheduleWeb/SQL/Connection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace FamilyScheduleWeb
+{
+    public class Connection
+    {
+
+        public static SqlConnection InitDBConnection()
+        {
+            return new SqlConnection("Server=192.185.6.102; Database=hoverhub_PRD; User Id=hoverhub_Admin; Password=Aintn0sucka!; Encrypt=false;");
+        }
+
+    }
+}
